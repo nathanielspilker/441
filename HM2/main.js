08596e92635c7c1d60aafc2a05be916b710e0685
nathanielspilker <legospilker@icloud.com>
2023-01-30 19:36:24 -0700

    Update main.js

--- HM2/main.js
+++ HM2/main.js
@@ -1,1 +1,10 @@
+// Oh my goodness I can't beleive I nearly forgot to do this!
+
+// I nearly had a panik attack when my cat ate a magnet. Turns out, he threw it up immidietly.
+
+/* I wonder what is your favorite music? What's your favorite game?
+We may never get the chance to know. I always enjoy rambling about
+my passions. It sometimes becomes a little bit of a problem, but no
+one complains */
+
 console.log("My three favorite websites are gamesworkshop, amazon, and youtube. My three favorite games are Terraria, Darksouls, and Pictionary. My three favorite artists are The Beatles, Queen, and Michaelangelo. Each of these artists have had profound and imapactful works in their times and revolutionized their fields.");
