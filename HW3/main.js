8cfcf7abb1168eeb74c309a4e38a8447ab62a25d
nathanielspilker <legospilker@icloud.com>
2023-02-06 19:42:47 -0700

    update

--- HW3/main.js
+++ HW3/main.js
@@ -2,9 +2,9 @@ function storyFunction(choice) {
     var answer1 = document.getElementById("choice1").innerHTML;
     var answer2 = document.getElementById("choice2").innerHTML;
     if (choice == 1 && answer1 == "Yes") {
-        document.getElementById("story").innerHTML = "Pugster continues into the park with confidence. Pugster meets another dog. What Pugster do now?";
-        document.getElementById("choice1").innerHTML = "Meet others";
-        document.getElementById("choice2").innerHTML = "Walk alone";
+        document.getElementById("story").innerHTML = "The heroes delve into the darkness encountering a giant slime cube! What now!?";
+        document.getElementById("choice1").innerHTML = "Avoid It";
+        document.getElementById("choice2").innerHTML = "Attack";
     } else if (choice == 2 && answer2 == "No") {
         document.getElementById("story").innerHTML = "Pugster starts to back out, but then something in the back of Pugster's mind says 'Wait!'";
         document.getElementById("choice1").innerHTML = "Go ahead and be brave";
