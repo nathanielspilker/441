da8d14c50766bc4244d5ea13f7bbb53c0ea5fdca
nathanielspilker <legospilker@icloud.com>
2023-03-06 22:23:02 -0700

    Homework 6 updates

--- HW5/main.js
+++ HW5/main.js
@@ -1,12 +1,17 @@
-var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+var imageNames = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
 var blankImagePath = "QuestionMark.jpg";
+var firstNumber = -1;
+var secondNumber = -1;
+var score = 0;
+var allFound = 0;
+var player = {"firstname":"", "lastname":"", "age":0, "score":0};
 var actualImages = new Array();
 function printBlanks()
 {
     createRandomImageArray();
-    for(var i = 0; i < imageTags.length; i++)
+    for(var i = 0; i < imageNames.length; i++)
     {
-        document.getElementById(imageTags[i]).src= blankImagePath;
+        document.getElementById(imageNames[i]).src= blankImagePath;
     }
 }
 function createRandomImageArray()
@@ -23,8 +28,65 @@ function createRandomImageArray()
         }
     }
 }
-
 function flipImage(number)
 {
-    document.getElementById(imageTags[number]).src= actualImages[number];
+    if(firstNumber >= 0)
+    {
+        secondNumber = number;
+        document.getElementById(imageNames[number]).src = actualImages[secondNumber];
+    }
+    else if(firstNumber < 0)
+    {
+        firstNumber = number;
+        document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
+    }
+    if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        score++;
+        setTimeout(imagesDisappear, 1000);
+    }
+    else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        score++;
+        allFound++;
+        firstNumber = -1;
+        secondNumber = -1;
+        if(allFound == actualImages.length/2)
+        {
+            player.score = score;
+            localStorage.setItem("playerInfo", JSON.stringify(player));
+            window.location = "end.html";
+        }
+    }
+}
+function imagesDisappear()
+{
+    console.log(firstNumber);
+    document.getElementById(imageNames[firstNumber]).src = blankImagePath;
+    document.getElementById(imageNames[secondNumber]).src = blankImagePath;
+    firstNumber = -1;
+    secondNumber = -1;
+}
+function addToPlayer()
+{
+    var firstName = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var age = document.getElementById("txtAge").value;
+    player.firstname = firstName;
+    player.lastname = lastName;
+    player.age = age;
+    localStorage.setItem("playerInfo", JSON.stringify(player));
+    window.location = "index.html";
+}
+function playerInfo()
+{
+    var playerInformation = localStorage.getItem("playerInfo");
+    player = JSON.parse(playerInformation);
+    var str = "Name: " + player.firstname + " " + player.lastname + "<br>" +
+    "Age: " + player.age + "<br>" +
+    "Score: " + player.score;
+    if(document.getElementById("endInformation") != null)
+    {
+        document.getElementById("endInformation").innerHTML = str;
+    }
 }
