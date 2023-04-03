d7702fdbb9fca9f9827cfde1ee4b38be31600b67
nathanielspilker <legospilker@icloud.com>
2023-04-02 18:53:29 -0600

    test 10

--- HW9/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("button").click(function () {
-    $("#bikeInformation").load(
-      "https://github.com/nathanielspilker/441/blob/main/HW9/bikeinfo.txt"
-    );
-  });
-});
