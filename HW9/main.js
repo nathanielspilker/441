7979b4b24c50d93174007e8daf33ddcbc61c8d60
nathanielspilker <legospilker@icloud.com>
2023-04-02 18:33:42 -0600

    test 8

--- HW9/main.js
+++ HW9/main.js
@@ -1,36 +1,7 @@
-let bike = {
-  manufacturer: "Santa Cruz",
-  model: "5010",
-  owner: {
-    firstName: "Bob",
-    lastName: "Jones"
-  },
-  sizes: ["small", "medium", "large", "x-large"]
-};
-
-$(function () {
+$(document).ready(function () {
   $("button").click(function () {
-    showBikeInfo();
+    $("#bikeInformation").load(
+      "https://montana-media-arts.github.io/441-WebTech-Spring2021-Examples/Week%209/bikeInfo.txt"
+    );
   });
 });
-
-function showBikeInfo() {
-  $("#bikeInformation").html(
-    "Manufacturer: " +
-      bike.manufacturer +
-      "<br>Model:" +
-      bike.model +
-      "<br>First Name:" +
-      bike.owner.firstName +
-      "<br>Last Name:" +
-      bike.owner.lastName +
-      "<br>Sizes Available:<br>" +
-      bike.sizes[0] +
-      "<br>" +
-      bike.sizes[1] +
-      "<br>" +
-      bike.sizes[2] +
-      "<br>" +
-      bike.sizes[3]
-  );
-}
