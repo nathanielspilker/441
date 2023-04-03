8a2b0338c7753d9dc38bfa5db4d97547e63a471e
nathanielspilker <legospilker@icloud.com>
2023-04-02 18:36:01 -0600

    update 9

--- HW9/main.js
+++ HW9/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("button").click(function () {
     $("#bikeInformation").load(
-      "https://montana-media-arts.github.io/441-WebTech-Spring2021-Examples/Week%209/bikeInfo.txt"
+      "https://github.com/nathanielspilker/441/blob/main/HW9/bikeinfo.txt"
     );
   });
 });
